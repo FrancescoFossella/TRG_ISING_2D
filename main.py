82920ec5dfc638c8d177fd9d2d9b7c36bd841c91
Francesco Fossella <francesco@francesco-machine.enst.fr>
2024-05-21 15:26:45 +0200

    new main

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from code.TRG import TRG
-
+import matplotlib.pyplot as plt 
 import numpy as np
 
-trg = TRG(2, 1, truncate=10)
 
-Z = trg.solve()
+(N ,truncate)= (10, 6)
 
-print(Z)
+beta_values=np.arange(0.01,2,0.001)
+free_energy=[]
+for beta in beta_values : 
+    trg = TRG(N, beta, truncate)  
+    Z = trg.solve()
+    free_energy.append(np.log(Z))
+
+plt.plot(beta_values,free_energy)
+# Add labels and a legend
+plt.xlabel(r'$\beta J$')
+plt.ylabel(r'$-\beta U$')
+plt.title(r'Internal energy density $-\beta \, U$')
+plt.legend()
+plt.subplots_adjust(wspace=0.25, hspace=0.15, left=0.07, right=0.98, top=0.95, bottom=0.1)  
+plt.savefig("energy.pdf",dpi=4000)
